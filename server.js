f5d9ff7838b2d65650c0dfbec10b465bc15b9e47
nftrepo2 <activegraceisworking@gmail.com>
2025-08-02 14:44:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // DB config
-const db = 'mongodb+srv://pius1:pius123@webdevelopment.xav1dsx.mongodb.net/cashapp-net';
+
+const db = 'mongodb+srv://marcelpolocha1:081358pius@cluster0.f9a85hv.mongodb.net/cashapp-net';
 mongoose.connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
